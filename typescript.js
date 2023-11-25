4a1931e209ca87b14eeb2234b049e5a0a85f4653
Rafael98-stack <rafaelcasapaojr@gmail.com>
2023-11-25 12:01:51 +0100

    Html Developing

--- typescript.js
+++ typescript.js
@@ -1,5 +1,7 @@
 var User = /** @class */ (function () {
     function User(_nome, _carica, _numerochiamate, _costochiamata) {
+        if (_carica === void 0) { _carica = 0; }
+        if (_numerochiamate === void 0) { _numerochiamate = 0; }
         if (_costochiamata === void 0) { _costochiamata = 0.2; }
         this.carica = 0;
         this.numerochiamate = 0;
@@ -33,32 +35,64 @@ var User = /** @class */ (function () {
     User.prototype.azzerachiamate = function () {
         this.numerochiamate = 0;
     };
+    User.prototype.getcontenitore = function () {
+        var contenitore = document.getElementById("contenitore");
+        if (contenitore) {
+            contenitore.innerHTML = "\n        \n        <p> ".concat(this.nome, "</p>\n        <p>ricarica ").concat(this.carica, "</p>\n        ");
+        }
+        else {
+            console.log("error");
+        }
+    };
     return User;
 }());
-var Utente1 = new User("Marco", 20, 5, this._costochiamata);
-console.log("Utente1");
-console.log(Utente1.nome);
-console.log("Ricarica effettua, di: ".concat(Utente1.carica, "$"));
-console.log("Numero di chiamate: ".concat(Utente1.getnumerochiamate()));
-console.log("Costo della chiamata: ".concat(Utente1.numero404(), "$"));
-console.log("Cancellazione Numeri Chiamate...");
-Utente1.azzerachiamate();
-console.log("Numero di chiamate: ".concat(Utente1.getnumerochiamate()));
-var Utente2 = new User("Luca", 30, 10, this._costochiamata);
-console.log("Utente2");
-console.log(Utente2.nome);
-console.log("Ricarica effettua, di: ".concat(Utente2.carica, "$"));
-console.log("Numero di chiamate: ".concat(Utente2.getnumerochiamate()));
-console.log("Costo della chiamata: ".concat(Utente2.numero404(), "$"));
-console.log("Cancellazione Numeri Chiamate...");
-Utente2.azzerachiamate();
-console.log("Numero di chiamate: ".concat(Utente2.getnumerochiamate()));
-var Utente3 = new User("Mario", 5, 1, this._costochiamata);
-console.log("Utente3");
-console.log(Utente3.nome);
-console.log("Ricarica effettua, di: ".concat(Utente3.carica, "$"));
-console.log("Numero di chiamate: ".concat(Utente3.getnumerochiamate()));
-console.log("Costo della chiamata: ".concat(Utente3.numero404(), "$"));
-console.log("Cancellazione Numeri Chiamate...");
-Utente3.azzerachiamate();
-console.log("Numero di chiamate: ".concat(Utente3.getnumerochiamate()));
+// const Utente1 = new User ("Marco", 20, 5, this._costochiamata);
+// console.log("Utente1");
+// console.log(Utente1.nome);
+// console.log(`Ricarica effettua, di: ${Utente1.carica}$`);
+// console.log(`Numero di chiamate: ${Utente1.getnumerochiamate()}`);
+// console.log(`Costo della chiamata: ${Utente1.numero404()}$`);
+// console.log(`Cancellazione Numeri Chiamate...`)
+// Utente1.azzerachiamate();
+// console.log(`Numero di chiamate: ${Utente1.getnumerochiamate()}`);
+// const Utente2 = new User ("Luca", 30, 10, this._costochiamata);
+// console.log("Utente2");
+// console.log(Utente2.nome);
+// console.log(`Ricarica effettua, di: ${Utente2.carica}$`);
+// console.log(`Numero di chiamate: ${Utente2.getnumerochiamate()}`);
+// console.log(`Costo della chiamata: ${Utente2.numero404()}$`);
+// console.log(`Cancellazione Numeri Chiamate...`)
+// Utente2.azzerachiamate();
+// console.log(`Numero di chiamate: ${Utente2.getnumerochiamate()}`);
+// const Utente3 = new User ("Mario", 5, 1, this._costochiamata);
+// console.log("Utente3");
+// console.log(Utente3.nome);
+// console.log(`Ricarica effettua, di: ${Utente3.carica}$`);
+// console.log(`Numero di chiamate: ${Utente3.getnumerochiamate()}`);
+// console.log(`Costo della chiamata: ${Utente3.numero404()}$`);
+// console.log(`Cancellazione Numeri Chiamate...`)
+// Utente3.azzerachiamate();
+// console.log(`Numero di chiamate: ${Utente3.getnumerochiamate()}`);
+var Utente1 = new User("Marco", this._carica, this._numerochiamate, this._costochiamata);
+function Ricarica() {
+    // const invioRicarica = document.getElementById("invioRicarica") as HTMLButtonElement
+    var ricarica = document.getElementById("ricarica");
+    var totRicarica = parseInt(ricarica.value) || 0;
+    Utente1.ricarica(totRicarica);
+    Utente1.getcontenitore();
+}
+function Chiamata() {
+    var durataMinuti = document.getElementById("chiamata");
+    var durata = parseInt(durataMinuti.value) || 0;
+    var i = 0;
+    var intervallo = setInterval(function () {
+        if (Utente1.carica > 0 && i < durata) {
+            Utente1.chiamata(i);
+            i++;
+            Utente1.getcontenitore();
+        }
+        else {
+            clearInterval(intervallo);
+        }
+    }, 1000);
+}
--- typescript.ts
+++ typescript.ts
@@ -17,7 +17,7 @@ class User implements Smartphone {
 carica: number = 0;
 numerochiamate: number = 0;
 costochiamata: number = 0.2;
-constructor (_nome: string, _carica: number, _numerochiamate: number, _costochiamata: number = 0.2) {
+constructor (_nome: string, _carica: number = 0, _numerochiamate: number = 0, _costochiamata: number = 0.2) {
 this.nome = _nome;
 this.carica = _carica;
 this.numerochiamate = _numerochiamate;
@@ -50,43 +50,80 @@ getnumerochiamate(): number {
 azzerachiamate(): void {
     this.numerochiamate = 0;
 }
+
+getcontenitore(): void {
+    const contenitore = document.getElementById("contenitore");
+    if (contenitore) {
+        contenitore.innerHTML = `
+        
+        <p> ${this.nome}</p>
+        <p>ricarica ${this.carica}</p>
+        `
+    } else {
+        console.log("error")
+    }
+}
 }
 
-const Utente1 = new User ("Marco", 20, 5, this._costochiamata);
+// const Utente1 = new User ("Marco", 20, 5, this._costochiamata);
 
-console.log("Utente1");
-console.log(Utente1.nome);
-console.log(`Ricarica effettua, di: ${Utente1.carica}$`);
-console.log(`Numero di chiamate: ${Utente1.getnumerochiamate()}`);
-console.log(`Costo della chiamata: ${Utente1.numero404()}$`);
-console.log(`Cancellazione Numeri Chiamate...`)
-Utente1.azzerachiamate();
-console.log(`Numero di chiamate: ${Utente1.getnumerochiamate()}`);
+// console.log("Utente1");
+// console.log(Utente1.nome);
+// console.log(`Ricarica effettua, di: ${Utente1.carica}$`);
+// console.log(`Numero di chiamate: ${Utente1.getnumerochiamate()}`);
+// console.log(`Costo della chiamata: ${Utente1.numero404()}$`);
+// console.log(`Cancellazione Numeri Chiamate...`)
+// Utente1.azzerachiamate();
+// console.log(`Numero di chiamate: ${Utente1.getnumerochiamate()}`);
 
-const Utente2 = new User ("Luca", 30, 10, this._costochiamata);
+// const Utente2 = new User ("Luca", 30, 10, this._costochiamata);
 
-console.log("Utente2");
-console.log(Utente2.nome);
-console.log(`Ricarica effettua, di: ${Utente2.carica}$`);
-console.log(`Numero di chiamate: ${Utente2.getnumerochiamate()}`);
-console.log(`Costo della chiamata: ${Utente2.numero404()}$`);
-console.log(`Cancellazione Numeri Chiamate...`)
-Utente2.azzerachiamate();
-console.log(`Numero di chiamate: ${Utente2.getnumerochiamate()}`);
+// console.log("Utente2");
+// console.log(Utente2.nome);
+// console.log(`Ricarica effettua, di: ${Utente2.carica}$`);
+// console.log(`Numero di chiamate: ${Utente2.getnumerochiamate()}`);
+// console.log(`Costo della chiamata: ${Utente2.numero404()}$`);
+// console.log(`Cancellazione Numeri Chiamate...`)
+// Utente2.azzerachiamate();
+// console.log(`Numero di chiamate: ${Utente2.getnumerochiamate()}`);
 
-const Utente3 = new User ("Mario", 5, 1, this._costochiamata);
+// const Utente3 = new User ("Mario", 5, 1, this._costochiamata);
 
-console.log("Utente3");
-console.log(Utente3.nome);
-console.log(`Ricarica effettua, di: ${Utente3.carica}$`);
-console.log(`Numero di chiamate: ${Utente3.getnumerochiamate()}`);
-console.log(`Costo della chiamata: ${Utente3.numero404()}$`);
-console.log(`Cancellazione Numeri Chiamate...`)
-Utente3.azzerachiamate();
-console.log(`Numero di chiamate: ${Utente3.getnumerochiamate()}`);
+// console.log("Utente3");
+// console.log(Utente3.nome);
+// console.log(`Ricarica effettua, di: ${Utente3.carica}$`);
+// console.log(`Numero di chiamate: ${Utente3.getnumerochiamate()}`);
+// console.log(`Costo della chiamata: ${Utente3.numero404()}$`);
+// console.log(`Cancellazione Numeri Chiamate...`)
+// Utente3.azzerachiamate();
+// console.log(`Numero di chiamate: ${Utente3.getnumerochiamate()}`);
+
+const Utente1 = new User ("Marco", this._carica, this._numerochiamate, this._costochiamata)
 
 
 
+ function Ricarica(): void {
+    // const invioRicarica = document.getElementById("invioRicarica") as HTMLButtonElement
+    const ricarica = document.getElementById("ricarica") as HTMLInputElement;
+  const totRicarica: number = parseInt(ricarica.value) || 0;
+Utente1.ricarica(totRicarica)
+Utente1.getcontenitore()
+}
+
+function Chiamata(): void {
+const durataMinuti = document.getElementById("chiamata") as HTMLInputElement;
+const durata: number = parseInt(durataMinuti.value) || 0;
+let i = 0;
+const intervallo = setInterval(() => {
+    if (Utente1.carica > 0 && i < durata) {
+Utente1.chiamata(i)
+i++;
+Utente1.getcontenitore();
+    } else {
+clearInterval(intervallo)
+    }
+}, 1000)
+}
 
 
 
